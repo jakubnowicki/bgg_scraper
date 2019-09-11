fb3005e4f9ddd72a1475d23cf9f7f8c61b8f4625
Jakub Nowicki <kuba@appsilon.com>
2019-09-11 09:59:54 +0200

    init db

---
+++ bgg/bgg/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, create_engine
+# from sqlalchemy.orm import relationship
+import os
+# from scrapy.utils.project import get_project_settings
+
+DeclarativeBase = declarative_base()
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+def db_connect():
+    return create_engine('sqlite:///' + os.path.join(basedir) +
+                                                    '/boardgames.db',
+                                                    echo=True)
+
+
+def create_table(engine):
+    DeclarativeBase.metadata.create_all(engine)
+
+
+class Boardgame(DeclarativeBase):
+    __tablename__ = 'boardgames'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    image = Column(String)
+    year = Column(Integer)
+
+    # daily_rating = relationship("DailyRating", back_populates="boardgames")
+
+    def __repr__(self):
+        return self.name
+
+
+class DailyRating(DeclarativeBase):
+    __tablename__ = "daily_rating"
+
+    id = Column(Integer, primary_key=True)
+    game_id = Column(Integer, ForeignKey('boardgames.id'))
+    rank = Column(Integer)
+    geek_rating = Column(Float)
+    avg_rating = Column(Float)
+    num_voters = Column(Integer)
+    timestamp = Column(String)
+
+    # boardgame = relationship("Boardgame", back_populates="daily_rating")
+
+    def __repr__(self):
+        return self.id
--- bgg/bgg/pipelines.py
+++ bgg/bgg/pipelines.py
@@ -5,7 +5,48 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+from sqlalchemy.orm import sessionmaker
+from bgg.models import db_connect, create_table, Boardgame, DailyRating
 
-class BggPipeline(object):
+
+class DBWriter(object):
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker.
+        Creates table.
+        """
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
     def process_item(self, item, spider):
+        game_id = item['image'].split('/')[2]
+
+        session = self.Session()
+
+        boardgamedb = Boardgame()
+        boardgamedb.id = game_id
+        boardgamedb.name = item['name']
+        boardgamedb.image = item['image']
+        boardgamedb.year = item['year']
+
+        dailyratingdb = DailyRating()
+        dailyratingdb.game_id = game_id
+        dailyratingdb.rank = item['rank']
+        dailyratingdb.geek_rating = item['geek_rating']
+        dailyratingdb.avg_rating = item['avg_rating']
+        dailyratingdb.num_voters = item['num_voters']
+        dailyratingdb.timestamp = item['timestamp']
+
+        try:
+            session.add(boardgamedb)
+            session.add(dailyratingdb)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
         return item
+
